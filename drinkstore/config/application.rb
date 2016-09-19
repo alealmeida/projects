add01ce6da4b9ebd6985db2fb12c5c3aad7b6997
alealmeida <ale.142@icloud.com>
2016-09-19 09:33:36 -0300

    inclusão: jade, sass, compass

--- drinkstore/config/application.rb
+++ drinkstore/config/application.rb
@@ -1,5 +1,5 @@
 require_relative 'boot'
-
+require 'bootstrap'
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -11,5 +11,6 @@ module Drinkstore
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+      config.compass.require "susy"
   end
 end
