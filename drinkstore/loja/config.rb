df25d1c848a69d3c801499de13518621f8559bd5
Ale <ale.142@icloud.com>
2016-10-12 13:10:43 -0300

    Download drinkstore
    Preparando o ambiente da Drinkstore com o que foi enviado pelo gpa

--- drinkstore/loja/config.rb
+++ drinkstore/loja/config.rb
@@ -1,15 +1,12 @@
-require 'compass/import-once/activate'
-# Require any additional compass plugins here.
-require 'bootstrap'
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
 sass_dir = "sass"
 images_dir = "images"
-javascripts_dir = "js"
+javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed #or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
